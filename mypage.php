dd49d45426d5c038abb5821296def10dbdce2ab1
ryunin <130955669+ryunin@users.noreply.github.com>
2023-09-04 12:15:01 +0900

    Create mypage.php

---
+++ mypage.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require_once '../classes/UserLogic.php';
+require_once '../functions.php';
+
+//　ログインしているか判定し、していなかったら新規登録画面へ返す
+$result = UserLogic::checkLogin();
+
+if (!$result) {
+  $_SESSION['login_err'] = 'ユーザを登録してログインしてください！';
+  header('Location: signup_form.php');
+  return;
+}
+
+$login_user = $_SESSION['login_user'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>catnet</title>
+</head>
+<body>
+<h2>マイページ</h2>
+<p>ログインユーザ：<?php echo h($login_user['name']) ?></p>
+<p>メールアドレス：<?php echo h($login_user['email']) ?></p>
+<form action="logout.php" method="POST">
+<input type="submit" name="logout" value="ログアウト">
+</form>
+<!-- ①ログアウト画面の作成 -->
+<!-- ②ログアウトメソッドの作成 -->
+<a href="./login.php">次の画面へ</a>
+</body>
+</html>
